b912d39aa940ea82b8a0a8dea886725744739f4f
sanal1989 <sanalsalynov@gmail.com>
2023-09-03 18:06:57 +0900

    3-1-4

--- src/main/java/habsida/spring/boot_security/demo/service/UserService.java
+++ src/main/java/habsida/spring/boot_security/demo/service/UserService.java
@@ -7,7 +7,7 @@ import java.util.List;
 
 public interface UserService {
     void add(User user);
-    User getById(long id);
+    User getById (long id);
     void updateUser(User user);
     void deleteById(long id);
     List<User> getUsers();
